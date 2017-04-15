ea88641e3bb1e46641b121651fcc69ad51900d18
Bruce Notario <brucelucien@gmail.com>
2017-04-15 00:26:37 -0300

    Iniciando implementação.

--- guru-app/src/app/app.component.ts
+++ guru-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'guru-root',
@@ -6,4 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  private guruForm: FormGroup = undefined;
+
+  public constructor(private formBuilder: FormBuilder) {
+    this.criarGuruForm();
+  }
+
+  private criarGuruForm(): void {
+    this.guruForm = this.formBuilder.group({
+      perguntaDoUsuario: '',
+      respostaDoGuru: ''
+    });
+    this.guruForm.get('respostaDoGuru').disable();
+  }
+
+  private onKeyDownPerguntaDoUsuario(event: KeyboardEvent) {
+    console.log(event.key);
+  }
+
 }
