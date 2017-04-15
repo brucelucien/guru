adf903e1c70e4f35ea289a7951c7afc9fd2c9254
Bruce Notario <brucelucien@gmail.com>
2017-04-15 15:59:48 -0300

    Ajustando teste e instalação do bootstrap.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { FormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        FormsModule
+      ],
       declarations: [
         AppComponent
-      ],
+      ]
     }).compileComponents();
   }));
 
